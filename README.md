b45ac4d74261b54691bd25eeb44718901da5aaa4
CJMoye <Cjmoye30@gmail.com>
2023-03-08 10:52:50 -0500

    Updated README and refactoring all files

--- README.md
+++ README.md
@@ -1,102 +1,30 @@
 # 06 Server-Side APIs: Weather Dashboard
 
-## Your Task
+## Description
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+- What was your motivation?
+    - To create a weather app which accepts in a user city, and then pulls the forecast for the current and next 5 days through the openweather API.
 
-Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+- Why did you build this project?
+    - This project was built to better understand the use of APIs and how versatile they can be in web development projects. It allows us to access a great amount of information for free/little cost
 
-**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+- What problem does it solve?
+    - We are able to see the forecast for any city selected for the current and next 5 days.
 
-You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+- What did you learn?
+    - JavaScript / JQuery:
+        - Better understanding of traversiting through the fetched data from the API. 
+        - Pulling unique values from data and storing those in an array in order to capture all of the unique data values.
+        - More practice creating and appending buttons
+        - Event Delegation - since the buttons are dynamically created, I was not able to put an event listener directly onto them. Instead, I had to put an event listener on the parent container (#buttonList) so that each of the created buttons would then have an event listener.
+        - More practice with local storage. Very easy to create and store an array into local storage which allows us to only have 1 key value, and then those values are all in the array and accessed by an index.
+        - localStorage.clear() - very easy to clear local storage
 
-## User Story
+## Installation / Live Site
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+[Github Pages Live Site]()
 
-## Acceptance Criteria
+## Usage / Screenshot
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
-
-## Mock-Up
-
-The following image shows the web application's appearance and functionality:
-
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
-
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2022 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+![Desktop](/assets/06Challenge%20-Weather-Dash-Desktop.png)
+![Mobile](/assets/06Challenge%20-Weather-Dash-Mobile.png)
--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 // HTML Element Selectors
 var city = $("#city-name");
 var date = $("#date");
-var temperature = $("#temp")
+var temperature = $("#temp");
 
 var cityArr = [];
 var key = "dfc53a3a9ff97d77b6af068616b52dc8&units";
 
-
+// Fetch weather for lat/lon based on user city input
 function returnUserWeather(lat, lon) {
 
-    // Refactored the code so that there are no additional arrays.
-    // The data is filtered based on the dates, and then the filtered data is then put into an array which is better/more sensible than increasing the index by 8 each time like I had the data before. This new refactor greatly reduces the amound of code and not that much harder to understand
-    // Each index of the array represents a new day
     fetch("https://api.openweathermap.org/data/2.5/forecast/?lat=" + lat + "&lon=" + lon + "&appid=" + key + "=imperial&cnt=40")
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
-            // console.log(data);
             
             var datesArray = [];
             var dataArr = [];
@@ -26,45 +22,36 @@ function returnUserWeather(lat, lon) {
             for (var i = 0; i < data.cnt; i++) {
                 datesArray.push(dayjs(data.list[i].dt_txt).format("YYYY-MM-DD"))
             }
-            // console.log(datesArray);
 
             // Method to pull all of the unique values from the dates we get
             var uniqueDatesArray = [...new Set(datesArray)];
-            console.log("Unique values:",uniqueDatesArray);
+            
+            // If unique values = 5, hide the display of the last forecast card
+            // The data pulled only shows 5 distinct days after a certain time - not sure if there is a possible workaround since we are limited to 40
+            if(uniqueDatesArray.length === 5) {
+                $("#fcst-day-5").attr("style", "display: none")
+            }
 
+            // Filtering all of the data given based on the unique dates, and placing them into a new data array
             for(var i = 0; i < uniqueDatesArray.length; i++) {
                 var dayData2 = data.list.filter(function(day) {
                     return day.dt_txt.includes(uniqueDatesArray[i])
                 })
-                console.log(dayData2);
                 dataArr.push(dayData2);
             }
-            
-            // Get data for all 6 days individually and put the data into a for loop
-            // for (var i = 0; i <=5; i++) {
-            //     var dayAdd = dayjs().add(i, "day").format("YYYY-MM-DD");
-            //     var dayData = data.list.filter(function(day) {
-            //         return day.dt_txt.includes(dayAdd);
-            //     })
-            //     // console.log(dayData);
-            //     dataArr.push(dayData);
-            // }
 
-            // The issue is that after a certain time - the data is not pulling correctly and it is reading undefined. I could go back to the MVP code and paste that back in if necessary, but I will try to keep things as is
-
-            // Looping through the next 5 days
-            // Pulling data from the first index of each of the arrays so that the data is consistent
-            // There is a potential to expand and include all of the information from these arrays, but that is not required at this moment.
+            // Looping through the data array and displaying the content in the HTML
             for (var i = 0; i <dataArr.length; i++) {
                 $("#fcst-day-" + i).children(".day-header").text(dayjs(dataArr[i][0].dt_txt).format("ddd MMM DD, YYYY"));
                 $("#fcst-day-" + i).children(".temp").text("Temperature: "+dataArr[i][0].main.temp);
-                $("#fcst-day-" + i).children(".wind").text("Wind Speed"+dataArr[i][0].wind.speed);
+                $("#fcst-day-" + i).children(".wind").text("Wind Speed: "+dataArr[i][0].wind.speed);
                 $("#fcst-day-" + i).children(".humidity").text("Humidity: "+dataArr[i][0].main.humidity);
                 $("#fcst-day-" + i).children(".icon").attr("src", "http://openweathermap.org/img/wn/" + dataArr[i][0].weather[0].icon + "@2x.png");
             }
         })
 }
 
+// Create buttons for a newly entered city
 function createButtons() {
     $("#buttonList").text("");
     for (var i = 0; i < cityArr.length; i++) {
@@ -75,25 +62,19 @@ function createButtons() {
     $("#clearStorage").attr("style", "display: block");
 }
 
+// On document load - checking local storage for any data to create buttons and appending.
+// If no data in local storage, weather defaults to Charlotte, NC
 $(function () {
-
-    // Event Delegation - since the city buttons are created after the documents lods, then we have to use event delegation and put the event listered to the entire list since that is already created. Then, we can capture the value which is clicked for items created dynamically.
+    // Event listener for city button clicks, which capture the city and run the searchCity function
     $("#buttonList").click(function (e) {
-        // Get the value of the previous city buttons and store that
-        // On click - pass this back into the handleCitySearch function and fetch the data
-        // console.log(e.target.innerHTML);
         userCity = e.target.innerHTML;
-        console.log(userCity);
         searchCity(userCity);
     })
 
     if (localStorage.length === 0) {
-        // run the function on default for Charlotte if no data exists already
         searchCity("Charlotte")
-        console.log("Local storae is empty!")
     } else {
         $("#clearStorage").attr("style", "display: block");
-
         var storedCities = JSON.parse(localStorage.getItem("cities"));
 
         // On document load - pulling the weather for the last searched city
@@ -102,6 +83,7 @@ $(function () {
         // Reinitialize the cityArray so that the buttons generate on load and do not get replaced with an empty array
         cityArr = JSON.parse(localStorage.getItem("cities"));
 
+        // Create and appending city buttons from local storage
         for (var i = 0; i < storedCities.length; i++) {
             var buttonEl = $("<button>").text(storedCities[i]).attr("class", "btn cityButton");
             $("#buttonList").append(buttonEl);
@@ -109,10 +91,10 @@ $(function () {
     }
 })
 
-// Running OpenWeather Geolocation
+$("#city-search-form").on("submit", handleCitySearch);
+// Fetching lat/lon based on user city before running function for weather forecasts
 function handleCitySearch(e) {
     e.preventDefault();
-
     userCity = $("#city-input").val();
 
     // Alret and end function if blank/existing city in storage
@@ -134,24 +116,22 @@ function handleCitySearch(e) {
             var userLat = data[0].lat;
             var userLon = data[0].lon;
             
+            // Update the city text
             city.text(data[0].name + ", " + data[0].state + " - " + data[0].country);
 
             // Pushing user data into an array which then goes to local storage for retrival
             cityArr.push(userCity);
 
+            // Create buttons and pull weather data with generated lan/lon
             createButtons();
             returnUserWeather(userLat, userLon);
         })
         .catch(function (err) {
-            cityArr.pop();
-            createButtons();
             alert("Please enter a valid name!");
         })
 }
 
-var form = $("#city-search-form");
-form.on("submit", handleCitySearch);
-
+// Function used for buttons clicked of existing cities
 function searchCity(xCity) {
     userCity = xCity;
     city.text(userCity);
@@ -173,7 +153,6 @@ function searchCity(xCity) {
 // Clear storage on button click
 $("#clearStorage").click(function(){
     cityArr = [];
-    console.log("Clear storage button was clicked");
     localStorage.clear();
     createButtons();
     $("#clearStorage").attr("style", "display: none");
